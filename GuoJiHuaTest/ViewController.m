c4e46ca8c1f9007fb056ebfbdefe27eb32d30b8a
xuyushiguang <xuyushiguang@163.com>
2017-02-21 21:47:52 +0800

    ewq
    New

--- GuoJiHuaTest/ViewController.m
+++ GuoJiHuaTest/ViewController.m
@@ -16,7 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 100, 40)];
+    label.text = NSLocalizedString(@"ok", nil);
+    [self.view addSubview:label];
+    
+    UILabel * label1 = [[UILabel alloc] initWithFrame:CGRectMake(50, 150, 100, 40)];
+    label1.text = [NSString stringWithFormat:NSLocalizedString(@"no", nil),@"sssssss"];
+    [self.view addSubview:label1];
+    
+    UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 200, 100, 100)];
+    [self.view addSubview:imageView];
+    imageView.image = [UIImage imageNamed:NSLocalizedString(@"im", nil)];
+    
+    UIImageView * imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(50, 310, 100, 100)];
+    [self.view addSubview:imageView1];
+    imageView1.image = [UIImage imageNamed:@"3.jpg"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
